45c1d8d0a2c15f5536df4cdce871408e76a32ced
Oktavian Yudha <pratamaoktavian66@gmail.com>
2021-08-12 09:17:05 +0700

    update db

--- database/majoo_test.sql
+++ database/majoo_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 10, 2021 at 01:17 PM
+-- Generation Time: Aug 12, 2021 at 04:16 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -31,7 +31,7 @@ CREATE TABLE `product` (
   `id_product` int(11) NOT NULL,
   `name_product` varchar(128) NOT NULL,
   `price` varchar(128) NOT NULL,
-  `detail_product` varchar(128) NOT NULL,
+  `detail_product` varchar(256) NOT NULL,
   `image_product` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -40,31 +40,10 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id_product`, `name_product`, `price`, `detail_product`, `image_product`) VALUES
-(5, 'Majoo Pro', '2.800.000', 'Majoo Pro adalah ...', 'standard_repo.png'),
-(6, 'Majoo Advance', '2.750.000', 'Majoo Advance adalah ..', 'paket-advance.png'),
-(7, 'Majoo Lifestyle', '3.000.000', 'Majoo Lifestyle adalah ...', 'paket-lifestyle.png'),
-(8, 'Majoo Desktop', '4.500.000', 'Majoo Dekstop adalah ..', 'paket-desktop.png');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_admin`
---
-
-CREATE TABLE `tbl_admin` (
-  `id` int(11) NOT NULL,
-  `fullname` varchar(128) NOT NULL,
-  `username` varchar(128) NOT NULL,
-  `image` varchar(128) NOT NULL,
-  `password` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tbl_admin`
---
-
-INSERT INTO `tbl_admin` (`id`, `fullname`, `username`, `image`, `password`) VALUES
-(2, 'User Admin', 'testadmin', 'default_image.svg', '$2y$10$YjUSCsHbNrc9KT7BRXrogOIHfQl43HNGptMCn2X8fSIjGRGDE7gM2');
+(5, 'Majoo Pro', '2.800.000', 'With the proper ingredients, success comes naturally. Here\'s everything you need to make a distinctive tasting sausage stick that will be sure to tingle your taste buds. Comes with complete instructions.', 'standard_repo.png'),
+(6, 'Majoo Advance', '2.750.000', 'Custom BioGenic Systems V5000-AB/C Isothermal Carousel Liquid Nitrogen Freezers offer liquid nitrogen storage temperatures without liquid nitrogen contact. Liquid nitrogen is stored in our patented jacketed space in the wall of the freezer.', 'paket-advance.png'),
+(7, 'Majoo Lifestyle', '3.000.000', 'Selected by House Beautiful Magazine as Best For The Bath. Our award-winning Natural Honey Facial Cleansing Bar is enriched with natural skin nourishing extracts, honey and royal jelly.', 'paket-lifestyle.png'),
+(8, 'Majoo Desktop', '4.500.000', 'At Hillbilly Stills we carry some of the best moonshine stills for sale you will find anywhere and our Turn Key Distillery is certainly no exception. This is the perfect whiskey, rum, and moonshine still kit for the serious distiller.', 'paket-desktop.png');
 
 --
 -- Indexes for dumped tables
@@ -77,12 +56,6 @@ ALTER TABLE `product`
   ADD PRIMARY KEY (`id_product`);
 
 --
--- Indexes for table `tbl_admin`
---
-ALTER TABLE `tbl_admin`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -91,12 +64,6 @@ ALTER TABLE `tbl_admin`
 --
 ALTER TABLE `product`
   MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `tbl_admin`
---
-ALTER TABLE `tbl_admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
