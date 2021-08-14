37c1976a7fce75fe315dc17a47f9b24420474322
Oktavian Yudha <pratamaoktavian66@gmail.com>
2021-08-14 08:37:01 +0700

    edit to make responsive stuff

--- application/views/product_home/index.php
+++ application/views/product_home/index.php
@@ -1,19 +1,18 @@
-<div class="container" style="margin-top: 25px; margin-bottom: 30px; ">
+<div class="container" style="margin-top: 25px; margin-bottom: 15px; ">
     <div style="margin-bottom: 20px;">
         <h2 class="text-white">Product</h2>
     </div>
     <div class="row">
-
         <?php foreach ($product as $prd) : ?>
-            <div class="col-lg mb-3">
-                <div style="margin-left: 15px;">
-                    <div class="card">
+            <div class="col-lg-3 col-md-6">
+                <div>
+                    <div class="card" style="margin-bottom: 30px;">
                         <img src="<?= base_url('assets/img/product/') . $prd['image_product']; ?>" class="card-img-top card-img align-content-sm-center">
-                        <div class="card-body ">
+                        <div class="card-body" style=" height:450px;">
                             <h5 class="card-title" style="text-align: center;"><b><?= $prd['name_product']; ?></b></h5>
                             <h6 class="card-subtitle" style="text-align: center;"><b>Rp. <?= $prd['price']; ?></b></h6>
                             <hr>
-                            <p class="card-text" style="text-align: center;"><?= $prd['detail_product']; ?></p>
+                            <p class="card-text" style="text-align: center; height: 250px;"><?= $prd['detail_product']; ?></p>
                             <hr>
                             <div class="text-center">
                                 <a href="<?= base_url('Product/detailorder/') . $prd['id_product']; ?>" class="btn btn-primary">Buy</a>
@@ -24,5 +23,4 @@
             </div>
         <?php endforeach ?>
     </div>
-
 </div>
--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,19 +1,20 @@
-<header class="navbar navbar-expand-md navbar-dark bd-navbar bg-gradient-success">
-  <nav class="container-xxl flex-wrap flex-md-nowrap" aria-label="Main navigation">
-    <a class="navbar-brand p-0 me-2" href="<?= base_url('product'); ?>">
-      <i class="fab fa-mastodon"></i>
-      | Majoo Teknologi Indonesia
-    </a>
+<header>
+	<nav class="navbar navbar-expand-md navbar-light bg-gradient-success">
+		<a href="<?= base_url('product'); ?>" class="navbar-brand text-white">
+			<i class="fab fa-mastodon" height="28"></i> | Majoo Teknologi Indonesia
+		</a>
+		<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-    <div class="collapse navbar-collapse" id="bdNavbar">
-       
-      <?php if ($user == null) : ?>
-        <a class="btn btn-bd-download d-lg-inline-block my-2 my-md-0 ms-md-3 text-white ms-md-auto" href="<?= base_url('auth'); ?>">Login</a>
-      <?php else : ?>
-        <a class="btn btn-bd-download d-lg-inline-block my-2 my-md-0 ms-md-3 text-white ms-md-auto" href="<?= base_url('admin'); ?>"><?= $user['fullname']; ?>  <img class="img-profile rounded-circle ml-1" style="width: 30px; height: 30px;" src="<?= base_url('assets/img/profile/') . $user['image']; ?>"></a>
-       
-      <?php endif; ?>
-
-    </div>
-  </nav>
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+			<div class="navbar-nav ml-auto">
+				<?php if ($user == null) : ?>
+					<a class="nav-item nav-link text-white" href="<?= base_url('auth'); ?>">Login</a>
+				<?php else : ?>
+					<a class="nav-item nav-link text-white" href="<?= base_url('admin'); ?>"><?= $user['fullname']; ?> <img class="img-profile rounded-circle ml-1" style="width: 30px; height: 30px;" src="<?= base_url('assets/img/profile/') . $user['image']; ?>"></a>
+				<?php endif; ?>
+			</div>
+		</div>
+	</nav>
 </header>
